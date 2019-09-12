db1ef4aa59095debe029f3bdefae19f9c9cd8218
Anthony Michael Stephens <mike@ThE-MacBook-Pro-7.local>
2019-09-12 16:00:34 -0400

    Added class names to use for Dom manipulation

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let input = document.querySelector("#display")
+let firstNum = input.value;
+
+
+
+
+function add(num) {
+    let input = document.querySelector("#display")
+    input.value = ""
+    
+    num +
+}
