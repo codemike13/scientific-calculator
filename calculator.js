70f03f68852ae769885c6c215026497dc43cdeef
Anthony Michael Stephens <mike@ThE-MacBook-Pro-7.local>
2019-09-16 07:37:47 -0400

    Got lost in the sauce

--- calculator.js
+++ calculator.js
@@ -7,12 +7,12 @@ const divideButton = document.querySelector(".divide");
 const nums = document.querySelectorAll(".num");
 const clearButton = document.querySelector(".clear");
 const memory = document.querySelector("#historyDisplay");
-let numStream = false;
+let NUM_STREAM = false;
 
 nums.forEach(e => {
   e.addEventListener("click", evt => {
-    if (!numStream) input.value = "";
-    numStream = true;
+    if (!NUM_STREAM) input.value = "";
+    NUM_STREAM = true;
     input.value += evt.target.innerText;
   });
 });
@@ -21,77 +21,94 @@ function calcFunc() {
   let obj = {
     valueHold: 0,
     currentValue: 0,
+    valFlag: false,
     memLength: 0,
     mathMemory: {
       memCount: 0,
       mem: []
     },
-    arithmeticPosition: [
-      { addition: false, addPlus: false },
-      { subtraction: false, subPlus: false },
-      { multiplication: false, multPlus: false },
-      { division: false, divplus: false }
+    position: [
+      { addition: false, last: 0, val: false },
+      { subtraction: false, last: 0, val: false },
+      { multiplication: false, last: 0, val: false },
+      { division: false, last: 0, val: false }
     ],
-
     add: function() {
-      this.arithmeticPosition.addition = true;
-      this.currentValue += Number(input.value);
-      input.value = this.currentValue;
-      numStream = false;
+      this.position[0].addition = true;
+      this.valueHold += Number(input.value);
+      input.value = this.valueHold;
+      NUM_STREAM = false; // Stop taking in numbers take full number typed.
+      this.currentValue = this.valueHold;
       input.value = input.value;
-      console.log(this.currentValue);
-
       equal.addEventListener("click", () => {
-        if (this.arithmeticPosition.addition) {
+        if (this.position[0].addition) {
           this.currentValue += Number(input.value);
           input.value = this.currentValue;
-
-          this.arithmeticPosition.addition = false;
+          this.valueHold = 0;
+          NUM_STREAM = false;
+          this.reset();
           console.log("Addition 'OFF'");
         }
       });
       console.log("Addition 'ON'");
     },
     sub: function() {
-      this.arithmeticPosition.subtraction = true;
-      this.valueHold = Number(input.value);
-      input.value = "";
+      this.position[1].subtraction = true;
+      if (this.valFlag) {
+        NUM_STREAM = true;
+        input.value = input.value;
+        this.valueHold -= input.value;
+      } else {
+        input.value = this.currentValue += Number(input.value * -1);
+        NUM_STREAM = false; // Stop taking in numbers take full number typed.
+        input.value = input.value;
+      }
       equal.addEventListener("click", () => {
-        if (this.arithmeticPosition.subtraction) {
-          this.currentValue = this.valueHold - Number(input.value);
+        if (this.position[1].subtraction) {
+          this.position[1].last = Number(input.value);
+          this.currentValue -= Number(input.value);
+          this.position[1].val = true;
           input.value = this.currentValue;
           this.valueHold = 0;
-          this.arithmeticPosition.subtraction = false;
+          NUM_STREAM = false;
+          this.reset();
           console.log("Subtraction 'OFF'");
         }
       });
       console.log("Subtraction 'ON'");
     },
     multiply: function() {
-      this.arithmeticPosition.multiplication = true;
-      this.valueHold = Number(input.value);
-      input.value = "";
+      this.position[2].multiplication = true;
+      this.valueHold += Number(input.value);
+      input.value = this.valueHold;
+      NUM_STREAM = false; // Stop taking in numbers take full number typed.
+      this.currentValue = this.valueHold;
+      input.value = input.value;
+
       equal.addEventListener("click", () => {
-        if (this.arithmeticPosition.multiplication) {
-          this.currentValue = this.valueHold * Number(input.value);
+        if (this.position[2].multiplication) {
+          this.position[2].last = Number(input.value);
+          this.position[2].val = true;
+          this.currentValue *= Number(input.value);
           input.value = this.currentValue;
           this.valueHold = 0;
-          this.arithmeticPosition.multiplication = false;
+          NUM_STREAM = false;
+          this.position[2].multiplication = false;
           console.log("Multiplication 'OFF'");
         }
       });
       console.log("Multiplication 'ON'");
     },
     divide: function() {
-      this.arithmeticPosition.division = true;
+      this.position.division = true;
       this.valueHold = Number(input.value);
       input.value = "";
       equal.addEventListener("click", () => {
-        if (this.arithmeticPosition.division) {
+        if (this.position.division) {
           this.currentValue = this.valueHold / Number(input.value);
           input.value = this.currentValue;
           this.valueHold = 0;
-          this.arithmeticPosition.division = false;
+          this.position.division = false;
           console.log("Division 'OFF'");
         }
       });
@@ -99,16 +116,28 @@ function calcFunc() {
     },
 
     clear: function() {
+      // if (this.position[0].val) {
+      //   this.currentValue -= this.position[0].last;
+      //   this.position[0].last = 0;
+      //   input.value = this.currentValue;
+      //   this.position[0].val = false;
+      // } else {
       this.currentValue = 0;
+      this.valueHold = 0;
+      this.reset;
       input.value = "";
+    },
+    reset: function() {
+      this.position[0].addition = false;
+      this.position[1].subtraction = false;
+      this.position[2].multiplication = false;
+      this.position[3].division = false;
     }
   };
 
   return obj;
 }
 const calc = calcFunc();
-
-let temp = 0;
 
 addButton.addEventListener("click", () => {
   calc.add();
